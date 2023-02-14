e8c91188fe71432752ee5248b830347db9bdf82a
sadat.malik <sadat.malik@me.com>
2023-02-14 09:35:15 +0000

    populate question blocks with data and styling

--- app.js
+++ app.js
@@ -93,3 +93,14 @@ const questions = [
         ]
     }
 ]
+
+const populateQuestions = () => {
+    questions.forEach(question => {
+        const titleBlock = document.createElement('div')
+        titleBlock.id = question.id
+        titleBlock.classList.add('title-block')
+
+        questionDisplay.append(titleBlock)
+    })
+}
+populateQuestions()
